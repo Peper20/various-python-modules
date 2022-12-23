8d3cf2221062ae561d2fd2cc8ea1c2b14607e7a5
Ivan Baluk <ivan.baluk@bk.ru>
2022-12-23 23:40:11 +0200

    version_2

--- binaryArrays.py
+++ binaryArrays.py
@@ -1,5 +1,11 @@
+# binary func tools beginning {
+
 def is_set_bit(number, index):
-	return (number & (1 << index)) != 0
+	return (number & (1 << index)) == 1
+
+def is_reset_bit(number, index):
+	return (number & (1 << index)) == 0
+
 
 def set_bit(number, index):
 	return number | (1 << index)
@@ -7,9 +13,27 @@ def set_bit(number, index):
 def reset_bit(number, index):
 	return number & ~(1 << index)
 
+
 def inverse_bit(number, index):
 	return number ^ (1 << index)
 
+# } binary func tools end
 
 
-#123
+
+# utilities func tools beginning {
+
+def unique_occurrences_bit_array(array):
+	bit_array = 0
+
+	for value in array:
+		bit_array = set_bit(bit_array, value)
+
+	return bit_array
+
+def unique_occurrences_count(array):
+	return unique_occurrences_bit_array(array).bit_count()
+
+
+
+# } utilities func tools end
