2912965119347cb49bd1b62be7576ecf59794f0a
Ivan Baluk <ivan.baluk@bk.ru>
2022-12-23 23:25:56 +0200

    version_1

--- binaryArrays.py
+++ binaryArrays.py
@@ -1,8 +1,3 @@
-from random import randint
-
-
-
-
 def is_set_bit(number, index):
 	return (number & (1 << index)) != 0
 
@@ -14,21 +9,3 @@ def reset_bit(number, index):
 
 def inverse_bit(number, index):
 	return number ^ (1 << index)
-
-
-
-# bit_array = 2 ** (n - 1)
-
-size = 5
-max_value = 5
-min_value = 0
-
-for i in range(10):
-	bit_array = 0
-	array = [randint(min_value, max_value) for i in range(size)]
-	print(array, end=' ')
-
-	for value in array:
-		bit_array = set_bit(bit_array, value)
-	
-	print('->', bit_array.bit_count())
